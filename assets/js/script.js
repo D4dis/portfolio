6ecf878387bc9f452a45b3d608717b2148265fc5
Dadis <raditztv@gmail.com>
2024-09-17 01:05:44 +0200

    search bar finished

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+let avaibleKeywords = [
+  'A propos de moi',
+  'Skills',
+  'Projets',
+  'Contact',
+];
+
+const resultsBox = document.querySelector(".result-box");
+const inputBox = document.getElementById("search");
+
+inputBox.onkeyup = function(){
+  let result = [];
+  let input = inputBox.value;
+  if(input.length){
+    result = avaibleKeywords.filter((keyword) => {
+      return keyword.toLowerCase().includes(input.toLowerCase());
+    });
+    console.log(result);
+  }
+  display(result);
+
+  if(!result.length){
+    resultsBox.innerHTML = '';
+  }
+}
+
+function display(result){
+  const content = result.map((list) => {
+    return "<li onclick=selectInput(this)>" + list + "</li>";
+  })
+  resultsBox.innerHTML = "<ul>" + content.join('') + "</ul>";
+}
+
+function selectInput(list){
+  inputBox.value = list.innerHTML
+}
